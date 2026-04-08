278093d1fb04b1a13c2ba61cf4bfcce44b61da8b
abdelhakim allouani <abdelhakimallouani@gmail.com>
2026-04-08 18:59:32 +0100

    Add role-based access control for Logement routes and implement RoleMiddleware

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next, $role): Response
+    {
+        if (Auth::user()->role === $role) {
+            abort(403, 'mochkil d role');
+        }
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\RoleMiddleware;
+use App\Http\Controllers\LogementController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +17,26 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::middleware('role:student')->group(function () {
+        Route::get('/logments',[LogementController::class,'index'])->name('logments.index');
+        Route::get('/logments/{logement}',[LogementController::class,'show'])->name('logments.show');
+
+    });
+
+    Route::middleware('role:owner')->group(function () {
+        Route::get('/mylogments',[LogementController::class,'myLogments'])->name('logments.index');
+        Route::get('/logments/create',[LogementController::class,'create'])->name('logments.create');
+        Route::post('/logments',[LogementController::class,'store'])->name('logments.store');
+        Route::get('/logments/{logement}/edit',[LogementController::class,'edit'])->name('logments.edit');
+        Route::put('/logments/{logement}',[LogementController::class,'update'])->name('logments.update');
+        Route::delete('/logments/{logement}',[LogementController::class,'destroy'])->name('logments.destroy');
+    });
+
+    Route::middleware('role:admin')->group(function () {
+        // admin route
+    });
 });
+
 
 require __DIR__.'/auth.php';
