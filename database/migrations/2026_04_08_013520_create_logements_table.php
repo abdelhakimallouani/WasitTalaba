9a47ac3939097a32df9cfce113de73a452719fd2
abdelhakim allouani <abdelhakimallouani@gmail.com>
2026-04-08 02:40:02 +0100

    Add models and migrations for Avis, Favori, Logement, LogementImage, Message, Reservation, and their respective tables

---
+++ database/migrations/2026_04_08_013520_create_logements_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('logements', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('titre');
+            $table->text('description');
+            $table->decimal('prix', 8, 2);
+            $table->string('type');
+            $table->string('ville');
+            $table->string('adresse');
+            $table->double('latitude')->nullable();
+            $table->double('longitude')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('logements');
+    }
+};
