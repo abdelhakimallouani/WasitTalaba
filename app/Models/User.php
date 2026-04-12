2a54785a78210259238de6d16f252734126618f1
abdelhakim allouani <abdelhakimallouani@gmail.com>
2026-04-13 00:52:44 +0100

    Add Message model relationships to User: implement sentMessages and receivedMessages methods for message handling.

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,13 @@ class User extends Authenticatable
         return $this->hasMany(Avis::class);
     }
 
+    public function sentMessages()
+    {
+        return $this->hasMany(Message::class, 'sender_id');
+    }
+    public function receivedMessages()
+    {
+        return $this->hasMany(Message::class, 'receiver_id');
+    }
+
 }
